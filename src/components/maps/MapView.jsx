777bf6304d5eae2390269ee72bec403d8c134ab1
Ufoex <57051039+Ufoex@users.noreply.github.com>
2022-05-06 00:35:30 -0500

    Update MapView.jsx

--- src/components/maps/MapView.jsx
+++ src/components/maps/MapView.jsx
@@ -30,7 +30,7 @@ const MapView = () => {
   }, [position]);
 
   return (
-    <MapContainer style={ { width: "100%", height: "900px" } } center={ [20.536937821169772, -103.96649936013992] } zoom={ 16 }>
+    <MapContainer style={ { width: "100%", height: "300px" } } center={ [20.536937821169772, -103.96649936013992] } zoom={ 16 }>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
